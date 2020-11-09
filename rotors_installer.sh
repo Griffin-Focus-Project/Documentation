ddaa244c6c8b5ff76015c576bf8a3fb67e7d1364
inauenma <72745082+inauenma@users.noreply.github.com>
2020-11-09 18:19:40 +0100

    Update rotors_installer.sh

--- rotors_installer.sh
+++ rotors_installer.sh
@@ -11,7 +11,7 @@ pip install future
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
-sudo apt-get install ros-melodic-joy ros-melodic-octomap-ros ros-melodic-mavlink python-wstool python-rosdep python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-melodic-control-toolbox ros-melodic-mavros -y
+sudo apt-get install ros-melodic-desktop-full ros-melodic-joy ros-melodic-octomap-ros ros-melodic-mavlink python-wstool python-rosdep python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-melodic-control-toolbox ros-melodic-mavros -y
 sudo rosdep init
 rosdep update
 source /opt/ros/melodic/setup.bash
