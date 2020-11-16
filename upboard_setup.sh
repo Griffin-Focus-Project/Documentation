f47b01e5b71f582d979e2d687b4dad6ede1d3977
morhues <58611929+morhues@users.noreply.github.com>
2020-11-16 17:44:23 +0100

    Update upboard_setup.sh

--- upboard_setup.sh
+++ upboard_setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install terminator
 # Install Ubuntu Kernel
 # From https://wiki.up-community.org/Ubuntu_18.04
 sudo add-apt-repository ppa:aaeon-cm/upboard
-sudp apt update
+sudo apt update
 sudo apt-get autoremove --purge 'linux-.*generic'
 sudo apt --fix-broken install
 sudo apt-get install linux-generic-hwe-18.04-edge-upboard
